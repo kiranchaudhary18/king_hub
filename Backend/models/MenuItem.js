ac7c500a7bed1691dc177dbe76e05d369006a8fa
Sujal Vasara <vasarasujal.cg@gmail.com>
2025-02-16 21:13:40 +0530

    Add new schema for particular Menuitems

--- Backend/models/MenuItem.js
+++ Backend/models/MenuItem.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const MenuItemSchema = new mongoose.Schema({
+    restaurant: String,
     foodname: String,
     category: Array
 });
 
 module.exports = mongoose.model('MenuItem', MenuItemSchema);
+
