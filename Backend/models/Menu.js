e40085e0207790fba2f326461051f7ec4a99dbd3
Sujal Vasara <vasarasujal.cg@gmail.com>
2025-02-17 16:17:36 +0530

    Change the schema according to the route

--- Backend/models/Menu.js
+++ Backend/models/Menu.js
@@ -1,12 +1,32 @@
 const mongoose = require('mongoose');
 
 const MenuSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    price: String,
-    restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant' },
-    category: String,
-    image: String
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    restaurantId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Restaurant',
+        required: true
+    },
+    category: {
+        type: String,
+        trim: true
+    },
+    image: {
+        type: String,
+        trim: true
+    }
 });
 
 module.exports = mongoose.model('Menu', MenuSchema);
