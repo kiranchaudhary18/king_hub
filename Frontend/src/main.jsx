f5b87ae21f85ce8d529ee100cb48f95dc2e14520
Sujal Vasara <vasarasujal.cg@gmail.com>
2025-03-04 23:11:35 +0530

    Integrated Auth0 authentication by adding Auth0Provider and add domain and clientId

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,9 +3,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = "dev-jpglhta0n2sfdq0v.us.auth0.com";
+const clientId = "wkVSPlMMCNZ47Pbc7MshqJp9Claw0RQu";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Auth0Provider domain={domain}
+      clientId={clientId}
+      authorizationParams={{ redirect_uri: window.location.origin }}>
+      <App />
+    </Auth0Provider>
   </StrictMode>,
 );
